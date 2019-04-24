c3203dd69d3c961285e9e528e56afdf50ddd399d
mewa <marcin.k.chmiel@gmail.com>
2019-04-25 00:40:52 +0200

    add test case including subpages and cycles

--- krawlr_test.go
+++ krawlr_test.go
@@ -5,15 +5,49 @@ import (
 	"testing"
 )
 
-func Test_Basic(t *testing.T) {
+func Test_Krawl_Basic(t *testing.T) {
 	k := New()
 
-	results, err := k.Crawl("http://localhost:8888")
+	results, err := k.Crawl("http://localhost:8888/")
 
 	expected := map[string]*LinkSet{
-		"http://localhost:8888": &LinkSet{
+		"http://localhost:8888/": &LinkSet{
 			"https://google.com":    true,
 			"https://wikipedia.org": true,
+		},
+	}
+
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	if !reflect.DeepEqual(results, expected) {
+		t.Fatalf("expected=%v, actual=%v\n", expected, results)
+	}
+}
+
+func Test_Krawl_Subpages(t *testing.T) {
+	k := New()
+
+	results, err := k.Crawl("http://localhost:8888/")
+
+	expected := map[string]*LinkSet{
+		"http://localhost:8888/": &LinkSet{
+			"https://google.com":             true,
+			"https://wikipedia.org":          true,
+			"http://localhost:8888/sub.html": true,
+		},
+		"http://localhost:8888/index.html": &LinkSet{
+			"https://google.com":             true,
+			"https://wikipedia.org":          true,
+			"http://localhost:8888/sub.html": true,
+		},
+		"http://localhost:8888/sub.html": &LinkSet{
+			"https://google.com":               true,
+			"https://wikipedia.org":            true,
+			"http://localhost:8888/":           true,
+			"http://localhost:8888/index.html": true,
+			"http://localhost:8888/x":          true,
 		},
 	}
 
